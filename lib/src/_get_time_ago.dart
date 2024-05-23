def19e2ad487340d0a08f1df1dcc8583f54bf650
T. Milian <50327780+tmilian@users.noreply.github.com>
2024-05-23 15:20:14 +0200

    fix: add locale to default DateFormat

--- lib/src/_get_time_ago.dart
+++ lib/src/_get_time_ago.dart
@@ -91,7 +91,7 @@ class GetTimeAgo {
     final _locale = locale ?? _defaultLocale;
     final _message = _messageMap[_locale] ?? EnglishMessages();
     final _pattern = pattern ?? "dd MMM, yyyy hh:mm aa";
-    final date = DateFormat(_pattern).format(dateTime);
+    final date = DateFormat(_pattern, _locale).format(dateTime);
     var _currentClock = DateTime.now();
     var elapsed =
         (_currentClock.millisecondsSinceEpoch - dateTime.millisecondsSinceEpoch)
